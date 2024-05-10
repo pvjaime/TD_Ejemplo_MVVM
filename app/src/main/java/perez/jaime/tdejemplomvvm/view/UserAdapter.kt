36c213462040135e60054f7e4044640494fe0806
Jaime Perez Varas <jaime.perez.varas@gmail.com>
2024-05-09 21:01:10 -0400

    Se creo el UserAdapter y el user_item_layout.xml

---
+++ app/src/main/java/perez/jaime/tdejemplomvvm/view/UserAdapter.kt
@@ -0,0 +1,39 @@
+package perez.jaime.tdejemplomvvm.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import perez.jaime.tdejemplomvvm.R
+import perez.jaime.tdejemplomvvm.model.User
+
+class UserAdapter(private val userList: List<User>) :
+    RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.user_item_layout, parent, false)
+        return UserViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = userList[position]
+        holder.bind(user)
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
+        private val emailTextView: TextView = itemView.findViewById(R.id.emailTextView)
+        fun bind(user: User) {
+            nameTextView.text = user.name
+            emailTextView.text = user.email
+        }
+    }
+
+
+}
